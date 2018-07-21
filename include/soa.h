3f69e3eb8ebdf069368857b436351848b235af69
Tim Quelch <tim@quelch.name>
2018-07-21 15:39:16 +1000

    Refactor tuple_apply

--- include/soa.h
+++ include/soa.h
@@ -7,9 +7,9 @@
 
 namespace soa {
     namespace {
-        template <typename Func, typename SizeType, SizeType... Is>
-        constexpr auto tuple_apply(Func&& f, std::index_sequence<Is...>) {
-            return f(std::integral_constant<SizeType, Is>{}...);
+        template <typename Func, std::size_t... Is>
+        constexpr auto tuple_apply_impl(Func&& f, std::index_sequence<Is...>) {
+            return f(std::integral_constant<std::size_t, Is>{}...);
         }
     } // namespace
 
@@ -25,7 +25,6 @@ namespace soa {
         using const_reference = std::tuple<const Ts&...>;
         using pointer = std::tuple<Ts*...>;
         using const_pointer = std::tuple<const Ts*...>;
-        using indices = std::index_sequence_for<Ts...>;
 
         template <size_type I>
         auto get() {
@@ -39,40 +38,42 @@ namespace soa {
 
         reference operator[](size_type i) {
             return tuple_apply(
-                [this, i](auto... Is) { return std::tie(std::get<Is>(vecs_)[i]...); }, indices{});
+                [this, i](auto... Is) { return std::tie(std::get<Is>(vecs_)[i]...); });
         }
 
         const_reference operator[](size_type i) const {
             return tuple_apply(
-                [this, i](auto... Is) { return std::tie(std::get<Is>(vecs_)[i]...); }, indices{});
+                [this, i](auto... Is) { return std::tie(std::get<Is>(vecs_)[i]...); });
         }
 
         void push_back(value_type val) {
-            tuple_apply(
-                [this, &val](auto... Is) {
-                    (std::get<Is>(vecs_).push_back(std::get<Is>(val)), ...);
-                },
-                indices{});
+            tuple_apply([this, &val](auto... Is) {
+                (std::get<Is>(vecs_).push_back(std::get<Is>(val)), ...);
+            });
         }
 
         size_type size() const { return std::get<0>(vecs_).size(); }
         bool empty() const { return size() == 0; }
 
         void reserve(size_type size) {
-            tuple_apply([this, size](auto... Is) { (std::get<Is>(vecs_).reserve(size), ...); },
-                        indices{});
+            tuple_apply([this, size](auto... Is) { (std::get<Is>(vecs_).reserve(size), ...); });
         }
 
         void clear() {
-            tuple_apply([this](auto... Is) { (std::get<Is>(vecs_).clear(), ...); }, indices{});
+            tuple_apply([this](auto... Is) { (std::get<Is>(vecs_).clear(), ...); });
         }
 
         void pop_back() {
-            tuple_apply([this](auto... Is) { (std::get<Is>(vecs_).pop_back(), ...); }, indices{});
+            tuple_apply([this](auto... Is) { (std::get<Is>(vecs_).pop_back(), ...); });
         }
 
     private:
         std::tuple<std::vector<Ts>...> vecs_;
+
+        template <typename Func>
+        static constexpr auto tuple_apply(Func&& f) {
+            return tuple_apply_impl(std::forward<Func>(f), std::index_sequence_for<Ts...>{});
+        }
     };
 } // namespace soa
 
